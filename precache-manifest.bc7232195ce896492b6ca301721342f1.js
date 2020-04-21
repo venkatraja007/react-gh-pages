752990df95390b6a69022a83bd1bf25ff2195db3
venkat <venkatesa.r@solvezy.com>
2020-04-21 23:09:31 +0530

    Updates

---
+++ precache-manifest.bc7232195ce896492b6ca301721342f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ca3bad696b78d98133e4604d834ae7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0d4837b56ce4df7a6c20",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "134def81b153d3636e4f",
+    "url": "/react-gh-pages/static/js/2.3c16eec8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.3c16eec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4837b56ce4df7a6c20",
+    "url": "/react-gh-pages/static/js/main.fd94b66a.chunk.js"
+  },
+  {
+    "revision": "b15fab85dea6ea389910",
+    "url": "/react-gh-pages/static/js/runtime-main.b1e97c2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
